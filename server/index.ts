bf3fdfadc9b09b5584c04730c01d96e4bce21472
Andrew <andrew.muloki@gmail.com>
2025-07-14 18:18:38 +0300

    Add trust proxy setting in server configuration to enhance security for proxied requests.

--- server/index.ts
+++ server/index.ts
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.set('trust proxy', 1); // trust first proxy
+
 const PgSession = pgSession(session);
 
 app.use(
